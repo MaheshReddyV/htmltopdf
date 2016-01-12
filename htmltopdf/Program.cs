6239777f6284cfaf70ec8aecad5565ab5f9a7252
Mahesh Reddy V <vmreddy1211@gmail.com>
2016-01-12 23:49:02 +0530

    HTML to pdf sample using Nreco Pdf generator
    This tool works better with css as well

---
+++ htmltopdf/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace htmltopdf
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string v="<!DOCTYPE html PUBLIC '-//W3C//DTD XHTML 1.0 Transitional//EN' 'http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd'><html xmlns='http://www.w3.org/1999/xhtml'><head></head><body><h1>WELCOME TO WORLD OF HTML to PDF</h1></body><html>          ";          
+            var pdfBytes = (new NReco.PdfGenerator.HtmlToPdfConverter()).GeneratePdf(v);
+            System.IO.File.WriteAllBytes("F:\\Work\\htmltopdf\\myfile.pdf", pdfBytes);
+        }   
+    }
+}
